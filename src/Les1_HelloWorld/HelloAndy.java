3185fcf3e984fb688f7495fc7101599ceee41776
Andrew Klokov <555135@mail.ru>
2021-08-06 15:29:49 +0300

    I wrote an elementary Hello World program in Java using the library

---
+++ src/Les1_HelloWorld/HelloAndy.java
@@ -0,0 +1,12 @@
+package Les1_HelloWorld;
+
+public class HelloAndy {
+
+    public static void main(String[] args) {
+        HelloAndyLib.One();
+        
+        for (int i = 0; i < args.length; ++i) {
+            System.out.printf("args[%d] = %s\n", i, args[i]);
+        }
+    }
+}
---
+++ src/Les1_HelloWorld/HelloAndyLib.java
@@ -0,0 +1,7 @@
+package Les1_HelloWorld;
+
+public class HelloAndyLib {
+    public static void One() {
+        System.out.println("What's up, Andy? (lib)");
+    }
+}
