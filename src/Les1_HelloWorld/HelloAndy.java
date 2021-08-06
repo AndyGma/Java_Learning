44462b37df403fa0b6dcd3608a6044bc4a9af258
Andrew Klokov <555135@mail.ru>
2021-08-06 15:37:39 +0300

    Init

--- src/Les1_HelloWorld/HelloAndy.java
+++ src/Les1_HelloWorld/HelloAndy.java
@@ -4,9 +4,10 @@ public class HelloAndy {
 
     public static void main(String[] args) {
         HelloAndyLib.One();
-        
+
         for (int i = 0; i < args.length; ++i) {
             System.out.printf("args[%d] = %s\n", i, args[i]);
         }
+        System.out.println("End Lesson. Thanks");
     }
 }
