f3e4f0145ff195cb99ab3f105eeeac42d9bdd91f
Andrew Klokov <555135@mail.ru>
2021-08-08 12:56:16 +0300

    Created the MyInteger class.java with getter and setter to get and set the value.

---
+++ src/main/java/Les2/MyInteger.java
@@ -0,0 +1,16 @@
+package Les2;
+
+public class MyInteger {
+
+    private int value;
+
+
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
