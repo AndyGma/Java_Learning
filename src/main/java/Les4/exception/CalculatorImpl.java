a1d594af34942084d61a221e344685117f6028f4
Andrew Klokov <555135@mail.ru>
2021-08-09 13:26:41 +0300

    Lesson 4

--- src/main/java/Les4/exception/CalculatorImpl.java
+++ src/main/java/Les4/exception/CalculatorImpl.java
@@ -1,10 +1,33 @@
 package Les4.exception;
 
+import java.util.Objects;
+
 public class CalculatorImpl implements Calculator{ // класс реализует интерфейс
+
+    /**
+     *
+     * @param expression
+     * @return
+     * @throws NullPointerException when <code>expression</code> is null
+     */
 
     @Override
     public double calculate(String expression) {
-        expression.toString();
+        Objects.requireNonNull(expression, "expression is null");
+//        if (expression == null) {
+//            throw new NullPointerException("expression is null");
+//        }
+
+        foo(expression);
         return 0;
+    }
+    private void foo(String expression) {
+        bar(expression);
+    }
+    private void bar(String expression) {
+        baz(expression);
+    }
+    private void baz(String expression) {
+        expression.toString();
     }
 }
