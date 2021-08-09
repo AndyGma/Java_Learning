7266fe78a8d52fbdddcba9a01067ac133a9217f1
Andrew Klokov <555135@mail.ru>
2021-08-09 15:16:28 +0300

    Lesson 4

--- src/main/java/Les4/exception/CalcMain.java
+++ src/main/java/Les4/exception/CalcMain.java
@@ -1,8 +1,20 @@
 package Les4.exception;
 
+import javax.imageio.stream.ImageInputStreamImpl;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class CalcMain {
 
     public static void main(String[] args) throws Exception {
-            new CalculatorImpl().calculate(null);
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            for (; ; ) {
+                System.out.println(">");
+                String expr = reader.readLine();
+                double result = new CalculatorImpl().calculate(expr);
+                System.out.println(">>>" + result);
+            }
+        }
     }
 }
